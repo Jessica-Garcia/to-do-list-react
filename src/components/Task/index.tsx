0aec2894a939fd885d17c92e0e67ba83c2ac2f46
Jéssica Garcia <jessica.rodriguesgarcia@gmail.com>
2022-10-28 10:02:18 -0300

    feat: add props and state in Task component

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,26 +1,34 @@
-import { RadioButton, Trash } from 'phosphor-react'
+import { Trash } from 'phosphor-react'
 import style from './style.module.css'
+interface TaskProps {
+    id: string,
+    content: string,
+    isComplete: boolean,
+    handleComplete: (id: string, isCompleteTask: boolean) => void
+}
 
-export const Task = () => {
+export const Task = ({id, content, isComplete, handleComplete}: TaskProps) => {
+
     return (
         <div className={style.taskContent}>
             <input 
                 type="checkbox" 
                 name="check" 
-                id="check"
+                id={id}
                 className={style.checkInput}
+                checked={isComplete}
+                onChange = {() => handleComplete(id, !isComplete)}
             />
-            <label htmlFor="check" className={style.title}>
+            <label htmlFor={id} className={style.title}>
                 <span className={style.checkSpan}></span>
                 <span className={style.titleSpan}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet in, fugit commodi odit et ut id omnis sequi. 
+                    {content}
                 </span>
             </label>
 
-        
             <button title='Delete' className={style.trashButton}>
                 <Trash size={16}/>
             </button>
-        </div>   
+        </div>  
     )
 }
